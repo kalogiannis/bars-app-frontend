754ddaa246a6c4612c89f8d34f42f4883e951f47
kalogiannis <85567795+kalogiannis@users.noreply.github.com>
2025-04-09 18:01:38 +0300

    fist commit

---
+++ src/pages/UserProfilePage.tsx
@@ -0,0 +1,25 @@
+import { useGetMyUser, useUpdateMyUser } from '@/api/MyUserApi'
+import UserProfileForm from '@/forms/UserProfileForm'
+
+const UserProfilePage = () => {
+  const {isLoading:isGetLoading, currentUser} = useGetMyUser()
+  const {isLoading:isUpdateLoading, updateUser} = useUpdateMyUser()
+  
+  if(isGetLoading){
+    return <span>loading...</span>
+  }
+  
+  if(!currentUser){
+    return <span>Unable to load user profile!</span>
+  }
+
+  return (
+    <UserProfileForm
+      currentUser={currentUser}
+      onSave={updateUser}
+      isLoading={isUpdateLoading}
+    />
+  )
+}
+
+export default UserProfilePage
